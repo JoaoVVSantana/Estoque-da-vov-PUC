822377f0cc73d389e105a8cedb415dd9a59ad7c8
Wesleyhrl <wesley_henrique15@hotmail.com>
2024-11-15 21:52:12 -0300

    update frontend pages estoque and novo produto

--- src/frontend/estoque_da_vovo/src/components/Btn/Btn.jsx
+++ src/frontend/estoque_da_vovo/src/components/Btn/Btn.jsx
@@ -3,9 +3,9 @@ import { Button } from 'react-bootstrap';
 
 import './Btn.css'
 
-export default function Btn({ text, icon, size }) {
+export default function Btn({ text, icon, size, onClick }) {
   return (
-    <Button className="button mb-4" variant='none' size={size}>
+    <Button className="button mb-4" variant='none' size={size} onClick={onClick}>
       {icon} {text}
     </Button>
   );
--- src/frontend/estoque_da_vovo/src/pages/Estoque/Estoque.jsx
+++ src/frontend/estoque_da_vovo/src/pages/Estoque/Estoque.jsx
@@ -3,20 +3,31 @@ import React from 'react';
 import Btn from '../../components/Btn/Btn.jsx';
 import TableToolbar from '../../components/TableToolbar/TableToolbar.jsx';
 import TableComponent from './../../components/TableComponent/TableComponent.jsx';
+import TitleContent from './../../components/TitleContent/TitleContent';
+import BreadCrumbNav from './../../components/BreadCrumbNav/BreadCrumbNav';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import TitleContent from './../../components/TitleContent/TitleContent';
-import BreadCrumbNav from './../../components/BreadCrumbNav/BreadCrumbNav';
+
+import { useNavigate } from "react-router-dom";
+
+
+
 
 
 
 export default function Estoque() {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/estoque/novo-produto");
+    };
+
     return (
         <>
             <BreadCrumbNav/>
-            <TitleContent title={"Estoque"}/>
-            <Btn text="Novo Produto" icon={<FontAwesomeIcon icon={faPlus} />}/>
+            <TitleContent title={"Estoque de Produtos"}/>
+            <Btn text="Novo Produto" icon={<FontAwesomeIcon icon={faPlus} />} onClick={handleClick}/>
             <TableToolbar />
             <TableComponent />
         </>
