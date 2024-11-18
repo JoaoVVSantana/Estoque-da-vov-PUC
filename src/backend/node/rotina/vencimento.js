c5f7ab458473fcc780d07de9bd9e5a3b074efd3a
João Victor Santana <jvictorvasconcellosantana@hotmail.com>
2024-11-18 18:56:34 -0300

    rotina de vencimento

---
+++ src/backend/node/rotina/vencimento.js
@@ -0,0 +1,17 @@
+import cron from 'node-cron';
+import axios from 'axios';
+
+
+cron.schedule('0 0 */14 * *', async () => { // Executa a cada 14 dias
+  console.log('Executando rotina de verificação de itens perto do vencimento.');
+
+  try {
+    
+    const response = await axios.get('http://localhost:5000/api/itensPertoDoVencimento'); // Essa é a API que roda o banco procurando os itens que estão vencendo
+
+    console.log(response.data);
+
+  } catch (error) {
+    console.error('Erro na rotina de itens perto do vencimento: ', error.message);
+  }
+});
